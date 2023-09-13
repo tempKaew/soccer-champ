bf120cefb9b08f66229c2a05ff404cd560ee4b42
Prasit.cpt <tempkaew@gmail.com>
2023-09-13 16:45:43 +0700

    fix type match

--- lib/line/event/reply-match-event.ts
+++ lib/line/event/reply-match-event.ts
@@ -1,27 +1,33 @@
-import { WebhookEvent, FlexMessage, MessageAPIResponseBase } from '@line/bot-sdk';
-import { getMatchToday } from '@lib/query';
-import { typeMatchFlex } from '@lib/types';
-import client from '@lib/line/client';
-import matchFlexMessage from '@line-message/match-flex-message';
+import {
+  WebhookEvent,
+  FlexMessage,
+  MessageAPIResponseBase
+} from '@line/bot-sdk'
+import { getMatchToday } from '@lib/query'
+import { match, typeMatchFlex } from '@lib/types'
+import client from '@lib/line/client'
+import matchFlexMessage from '@line-message/match-flex-message'
 
-export default async function replyMatchEvent (event: WebhookEvent): Promise<MessageAPIResponseBase | undefined> {
+export default async function replyMatchEvent(
+  event: WebhookEvent
+): Promise<MessageAPIResponseBase | undefined> {
   if (event.type !== 'message' || event.message.type !== 'text') {
-    return;
+    return
   }
-  const { replyToken } = event;
+  const { replyToken } = event
   // const { text } = event.message;
 
   const matchToday = await getMatchToday()
 
-  const mapMatches:(typeMatchFlex)[] = matchToday.map( match => {
+  const mapMatches: typeMatchFlex[] = matchToday.map((match: match) => {
     return {
       id: match.id,
-      "teamHomeName": match.teams_match_team_home_idToteams?.name ?? '',
-      "teamHomeImage": match.teams_match_team_home_idToteams?.image ?? '',
-      "teamVisitorName": match.teams_match_team_visitor_idToteams?.name ?? '',
-      "teamVisitorImage": match.teams_match_team_visitor_idToteams?.image ?? '',
-      "dateKickoff": match.date_time_kickoff,
-      "channel": match.channel_live ?? ''
+      teamHomeName: match.teams_match_team_home_idToteams?.name ?? '',
+      teamHomeImage: match.teams_match_team_home_idToteams?.image ?? '',
+      teamVisitorName: match.teams_match_team_visitor_idToteams?.name ?? '',
+      teamVisitorImage: match.teams_match_team_visitor_idToteams?.image ?? '',
+      dateKickoff: match.date_time_kickoff,
+      channel: match.channel_live ?? ''
     }
   })
 
@@ -31,6 +37,6 @@ export default async function replyMatchEvent (event: WebhookEvent): Promise<Mes
       altText: 'ทายผลบอล',
       contents: matchFlexMessage(mapMatches)
     }
-    await client.replyMessage(replyToken, response);
+    await client.replyMessage(replyToken, response)
   }
 }
--- lib/types.ts
+++ lib/types.ts
@@ -1,74 +1,89 @@
+import { type } from 'os'
+
 export type userPoint = {
-  id: bigint | null,
-  point: string,
-  match: string,
-  name: string | undefined,
+  id: bigint | null
+  point: string
+  match: string
+  name: string | undefined
   image: string | null | undefined
 }
 
 export type typeUserProfile = {
-  id: BigInt | undefined,
-  name: string | undefined,
+  id: BigInt | undefined
+  name: string | undefined
   image: string | null | undefined
 }
 
 export type typeMatchFlex = {
-  id: BigInt | null,
-  teamHomeName: string | null,
-  teamHomeImage: string | null,
-  teamVisitorName: string | null,
-  teamVisitorImage: string | null,
-  dateKickoff: Date | null,
-  channel: string | null,
+  id: BigInt | null
+  teamHomeName: string | null
+  teamHomeImage: string | null
+  teamVisitorName: string | null
+  teamVisitorImage: string | null
+  dateKickoff: Date | null
+  channel: string | null
 }
 
 export type typeMapMatchApi = {
-  status: string | null,
+  status: string | null
   home: {
-    id: number | null,
-    name: string | null,
-    goal: number | null,
-    winner:  boolean | null
-  },
+    id: number | null
+    name: string | null
+    goal: number | null
+    winner: boolean | null
+  }
   visitor: {
-    id: number | null,
-    name: string | null,
-    goal: number | null,
-    winner:  boolean | null
+    id: number | null
+    name: string | null
+    goal: number | null
+    winner: boolean | null
   }
 }
 
 export type typeMathWhoIs = {
-  id: bigint,
+  id: bigint
   home: {
-    id: bigint | undefined,
-    name: string | undefined,
-    image: string | undefined,
-    joiner: (typeUserProfile)[] | undefined
-  },
+    id: bigint | undefined
+    name: string | undefined
+    image: string | undefined
+    joiner: typeUserProfile[] | undefined
+  }
   visitor: {
-    id: bigint | undefined,
-    name: string | undefined,
-    image: string | undefined,
-    joiner: (typeUserProfile)[] | undefined
-  },
+    id: bigint | undefined
+    name: string | undefined
+    image: string | undefined
+    joiner: typeUserProfile[] | undefined
+  }
   draw: {
-    joiner: (typeUserProfile)[] | undefined
+    joiner: typeUserProfile[] | undefined
   }
 }
 
 export type typeTeamInfo = {
-  id: bigint,
-  image: string,
-  name: string,
+  id: bigint
+  image: string
+  name: string
   champ_score: number | null
 }
 
 export type typeChampWhoIs = {
-  id: bigint,
-  line_user_name: string,
-  line_user_image: string,
-  team_champ_name: string,
-  team_champ_image: string,
+  id: bigint
+  line_user_name: string
+  line_user_image: string
+  team_champ_name: string
+  team_champ_image: string
   point: number
+}
+
+export type match = {
+  id: BigInt | null
+  teams_match_team_home_idToteams: teamMatch | null
+  teams_match_team_visitor_idToteams: teamMatch | null
+  date_time_kickoff: Date | null
+  channel_live: string | null
+}
+
+export type teamMatch = {
+  name: string | null
+  image: string | null
 }
